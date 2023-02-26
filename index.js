bcb972c8e7ca9c61d65d06ccd3ed7e46ebfd95db
Klara Sarac <klarasarac55@gmail.com>
2023-02-26 18:24:37 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const hourEl=document.getElementById("hour")
-const minuteEl=document.getElementById("minutes")
-const secondEl=document.getElementById("seconds")
+const hourEl=document.getElementById('hour');
+const minuteEl=document.getElementById('minutes');
+const secondEl=document.getElementById('seconds');
 
 function updateClock(){
-    let h = new Date().getHours()
-    let m = new Date().getMinutes()
-    let s = new Date().getSeconds()
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
 
     h= h<10 ? "0" + h : h;
     m= m<10 ? "0" + m : m;
@@ -17,6 +17,6 @@ function updateClock(){
     secondEl.innerText = s;
     setTimeout(()=>{
      updateClock()
-    },1000)
+    },1000);
 }
- updateClock()
+ updateClock();
